8025ad381d758883492961ac7e76561efdaa7721
ArtemSharypov <artem.sharypov@gmail.com>
2017-04-23 18:00:58 -0500

    Map Changes
    Added bounds and centering of the map

--- app/src/main/java/com/artem/uofmcampusmap/MapFragment.java
+++ app/src/main/java/com/artem/uofmcampusmap/MapFragment.java
@@ -9,10 +9,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 /**
  * Created by Artem on 2017-04-21.
@@ -92,6 +96,15 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     {
         //centers map in center of campus
         //sets boundaries for the area
+        LatLng northEastCorner = new LatLng(49.814274, -97.131857);
+        LatLng southWestCorner = new LatLng(49.798710, -97.152484);
+        LatLngBounds uOfMCampus = new LatLngBounds(northEastCorner, southWestCorner);
+
+        googleMap.setLatLngBoundsForCameraTarget(uOfMCampus);
+
+        LatLng centerOfCampus = new LatLng(49.809496, -97.133810);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(centerOfCampus));
     }
 
     private void addBuildingEntranceMarkers()
